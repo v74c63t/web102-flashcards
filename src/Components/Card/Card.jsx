4a3763d7f329b181f3971d85f9a540a25ca5a5dd
Vanessa Tang <vanesst5@uci.edu>
2024-03-06 18:23:08 -0800

    Fixed spacing

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -2,19 +2,19 @@ import './Card.css'
 import { useState } from 'react'
 
 const Card = () => {
-    const [flip, setFlip] = useState(false)
-    return(
-        <div>
-            <div className="card" onClick={()=>setFlip(!flip)}>
-              {
-                flip ? <div className="back">back</div> :
-                <div className="front">front</div>
-              }
-            </div>
-            <button className="btn">prev</button>
-            <button className="btn">next</button>
-        </div>
-    )
+  const [flip, setFlip] = useState(false)
+  return(
+    <div>
+      <div className="card" onClick={()=>setFlip(!flip)}>
+        {
+          flip ? <div className="back">back</div> :
+          <div className="front">front</div>
+        }
+      </div>
+      <button className="btn">prev</button>
+      <button className="btn">next</button>
+    </div>
+  )
 }
 
 export default Card;
