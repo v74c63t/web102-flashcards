d4996baa83e3ac5682ca90ad0020979a5036986c
Vanessa Tang <vanesst5@uci.edu>
2024-03-08 03:03:03 -0800

    Slight change to props

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,18 +1,15 @@
 import './Card.css'
 import { useState } from 'react'
 
-const Card = ({question, answer}) => {
+const Card = ({card}) => {
   const [flip, setFlip] = useState(false)
+
   return(
-    <div>
-      <div className="card" onClick={()=>setFlip(!flip)}>
-        {
-          flip ? <div className="back">{answer}</div> :
-          <div className="front">{question}</div>
-        }
-      </div>
-      <button className="btn">prev</button>
-      <button className="btn">next</button>
+    <div className="card" onClick={()=>setFlip(!flip)}>
+      {
+        flip ? <div className="back">{card.answer}</div> :
+        <div className="front">{card.question}</div>
+      }
     </div>
   )
 }
