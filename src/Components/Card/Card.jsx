cdec8d3574e91b2a93b8d55ee6aa14c8e60d5702
Vanessa Tang <vanesst5@uci.edu>
2024-03-08 18:26:18 -0800

    Embedded some images/videos to card

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,12 +1,22 @@
 import './Card.css'
-import { useState } from 'react'
 
 const Card = ({card, flip, setFlip}) => {
 
   return(
     <div className={`card ${card.difficulty}`} onClick={()=>setFlip(!flip)}>
       {
-        flip ? <div className="back">{card.answer}</div> :
+        flip ? <div className="back">
+                {card.answer}
+                {card.answerImage != undefined ? <div> <img width={250} height={"auto"} src={card.answerImage} alt='answer image' /> </div> : ""}
+                {card.video != undefined ? (
+                  <div> 
+                    <iframe width="320" height="240"
+                      src={card.video}>
+                    </iframe>
+                  </div>
+                ) : ""}
+              </div> 
+              :
         <div className="front">{card.question}</div>
       }
     </div>
